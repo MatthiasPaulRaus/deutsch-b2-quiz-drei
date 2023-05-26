7dd210fd2ff21a2f4160dc5bd9804cbc71291077
matthias paul raus <81903294+MatthiasPaulRaus@users.noreply.github.com>
2023-05-26 16:53:31 +0200

    first commit

---
+++ src/QuestionsList.jsx
@@ -0,0 +1,221 @@
+const QuestionsList = [
+    {
+        question:"In unserem Haushalt leben sowohl mein Partner",
+        answersList: [
+            {answer:"als auch dessen Eltern.", isCorrect:true},
+            {answer:"als auch seine Nachbarn.", isCorrect:false},
+            {answer:"als auch meine Fernbeziehung.", isCorrect:false},
+        ]
+    },{
+        question:"Die Statistik gibt Auskunft",
+        answersList: [
+            {answer:"von den Großfamilien in der Schweiz.", isCorrect:false},
+            {answer:"über die Zahl der Einpersonenhaushalte in Deutschland.", isCorrect:true},
+            {answer:"der alleinstehenden Mitbewohner in Hamburg.", isCorrect:false},
+        ]
+    },{
+        question:"Je länger wir zusammen leben,",
+        answersList: [
+            {answer:"umso besser verstehen sie uns.", isCorrect:false},
+            {answer:"desto schlimmer ist ihre Beziehung.", isCorrect:false},
+            {answer:"desto besser verstehen wir uns.", isCorrect:true},
+        ]
+    },{
+        question:"Eine bikulturelle Beziehung",
+        answersList: [
+            {answer:"verbindet Menschen aus verschiedenen Kulturen.", isCorrect:true},
+            {answer:"findet in zwei verschiedenen Kulturen statt.", isCorrect:false},
+            {answer:"interessiert sich für mehrere Kulturen.", isCorrect:false},
+        ]
+    },{
+        question:"Leider haben nicht alle ein Verständnis",
+        answersList: [
+            {answer:"über die Nachbarschaftshilfe.", isCorrect:false},
+            {answer:"für die Probleme der Singles.", isCorrect:true},
+            {answer:"wegen der Chancengleichheit.", isCorrect:false},
+        ]
+    },{
+        question:"Bernhard sagt, seine Freundin",
+        answersList: [
+            {answer:"war gestern von ihm verlassen.", isCorrect:false},
+            {answer:"würde nächste Woche ihn verlassen.", isCorrect:false},
+            {answer:"habe ihn letzte Woche verlassen.", isCorrect:true},
+        ]
+    },{
+        question:"Der Experte erklärt,",
+        answersList: [
+            {answer:"das Bedürfnis nach Sicherheit sei ein Grund dafür.", isCorrect:true},
+            {answer:"die Notwendigkeit treibt die Menschen an.", isCorrect:false},
+            {answer:"die fehlende Verantwortung hat das Chaos verursacht.", isCorrect:false},
+        ]
+    },{
+        question:"Wer ohne Trauschein leben will,",
+        answersList: [
+            {answer:"hätte ja als Single gelebt.", isCorrect:false},
+            {answer:"der kann ja Single bleiben.", isCorrect:true},
+            {answer:"müsste daher Single werden.", isCorrect:false},
+        ]
+    },{
+        question:"Wem diese Form des Zusammenlebens nicht gefällt,",
+        answersList: [
+            {answer:"hat andere Formen wählen können.", isCorrect:false},
+            {answer:"könnte seinen Gefallen daran finden.", isCorrect:false},
+            {answer:"kann ja andere Formen ausprobieren.", isCorrect:true},
+        ]
+    },{
+        question:"Meine Großeltern feiern morgen Goldene Hochzeit.",
+        answersList: [
+            {answer:"Was, sie sind schon 50 Jahre verheiratet?", isCorrect:true},
+            {answer:"Echt, schon mehr als 50 Jahre zusammen?", isCorrect:false},
+            {answer:"Unglaublich, sie haben erst vor 15 Jahren geheiratet.", isCorrect:false},
+        ]
+    },
+    
+    {
+        question:"Ihre Kritik ist berechtigt,",
+        answersList: [
+            {answer:"ernst nehmen sollte man sie trotzdem nicht.", isCorrect:true},
+            {answer:"wir wüden Sie nie ernst nehmen.", isCorrect:false},
+            {answer:"aber wir haben die gleiche Meinung.", isCorrect:false},
+        ]
+    },{
+        question:"Auf Fleisch ganz zu verzichten,",
+        answersList: [
+            {answer:"ist bei Veganern nur teilweise akzeptabel.", isCorrect:false},
+            {answer:"sei die Absicht der Vegetarier.", isCorrect:true},
+            {answer:"kommt für Veganer gar nicht infrage.", isCorrect:false},
+        ]
+    },{
+        question:"Sofern Sie an unseren Ernährungstipps interessiert sind,",
+        answersList: [
+            {answer:"geben wir Ihre Tipps gerne online weiter.", isCorrect:false},
+            {answer:"werden wir Ihren Antrag schnell bearbeiten.", isCorrect:false},
+            {answer:"können wir Ihnen gerne unsere Broschüre schicken.", isCorrect:true},
+        ]
+    },{
+        question:"Falls ich kein Ersatzgerät von Ihnen bekomme,",
+        answersList: [
+            {answer:"wende ich mich an den Verbraucherschutz.", isCorrect:true},
+            {answer:"werde ich die Nachricht an die Verbraucherzentrale schicken.", isCorrect:false},
+            {answer:"benachrichtige ich zum Schutz die Verbraucher.", isCorrect:false},
+        ]
+    },{
+        question:"Schon beim Kauf dieser Produkte",
+        answersList: [
+            {answer:"sollen Sie zusätzlich auf die Stoffe achten.", isCorrect:false},
+            {answer:"sollten Sie auf die Zusatzstoffe achten.", isCorrect:true},
+            {answer:"Beachten Sie mögliche Konservierungsstoffe.", isCorrect:false},
+        ]
+    },{
+        question:"Bei Unzufriedenheit mit unseren Produkten",
+        answersList: [
+            {answer:"schützen wir die Verbraucher vor Betrug.", isCorrect:false},
+            {answer:"hilft Ihnen kein staatlicher Verbraucherschutz.", isCorrect:false},
+            {answer:"wenden Sie sich umgehend an den Kundenservice.", isCorrect:true},
+        ]
+    },{
+        question:"Das Mindesthaltbarkeitsdatum informiert",
+        answersList: [
+            {answer:"darüber, wie lange man das Produkt verzehren kann.", isCorrect:true},
+            {answer:"die Kunden darüber, wie lange sie zum Essen brauchen. ", isCorrect:false},
+            {answer:"darüber, wann Sie ein Produkt essen sollten.", isCorrect:false},
+        ]
+    },{
+        question:"Wir haben die Mayonnaise doch noch gegessen,",
+        answersList: [
+            {answer:"weil das Datum schon abgelaufen ist.", isCorrect:false},
+            {answer:"obwohl das Haltbarkeitsdatum schon abgelaufen war.", isCorrect:true},
+            {answer:"denn sie war nicht mehr genießbar.", isCorrect:false},
+        ]
+    },{
+        question:"Falls man mit dem Kundenservice unzufrieden ist,",
+        answersList: [
+            {answer:"reklamieren Sie nicht beim Kundendienst.", isCorrect:false},
+            {answer:"sollten Sie sofort die Polizei anrufen.", isCorrect:false},
+            {answer:"sollte man beim Verbraucherschutz reklamieren.", isCorrect:true},
+        ]
+    },{
+        question:"Die meisten Verpackungen aus Plastik sind vermeidbar.",
+        answersList: [
+            {answer:"Dennoch wird noch viel zu viel davon benutzt.", isCorrect:true},
+            {answer:"Trotzdem sollten Sie nicht darauf verzichten.", isCorrect:false},
+            {answer:"Obwohl es dafür keine Alternativen gibt.", isCorrect:false},
+        ]
+    },
+    
+
+    {
+        question:"Infolge der hohen Mietkosten",
+        answersList: [
+            {answer:"bevorzugen viele Studenten bei ihren Eltern zu wohnen.", isCorrect:true},
+            {answer:"wohnen viele Auszubildende in der Firma.", isCorrect:false},
+            {answer:"suchen viele Schüler eine Immobilienagentur.", isCorrect:false},
+        ]
+    },{
+        question:"Schon als Kind interessierte sie sich für Tiere.",
+        answersList: [
+            {answer:"Danach wollte sie Medizin für Tiere benutzen.", isCorrect:false},
+            {answer:"Infolgedessen studierte sie Tiermedizin.", isCorrect:true},
+            {answer:"Sodass sie später Medizin studieren wollte.", isCorrect:false},
+        ]
+    },{
+        question:"Mir wäre eine Wohngemeinschaft am liebsten.",
+        answersList: [
+            {answer:"Dann musst du alleine eine hohe Miete bezahlen.", isCorrect:false},
+            {answer:"Das kannst du dir gar nicht vorstellen.", isCorrect:false},
+            {answer:"Das kommt für mich gar nicht infrage.", isCorrect:true},
+        ]
+    },{
+        question:"Der Student hat einen langen Vortrag gehalten.",
+        answersList: [
+            {answer:"Er hat viele Aspekte kritisch betrachtet.", isCorrect:true},
+            {answer:"Er konnte alle Anwesenden kritisieren.", isCorrect:false},
+            {answer:"Er wollte sich nur einen allgemeinen Überblick verschaffen.", isCorrect:false},
+        ]
+    },{
+        question:"Bei Ihrem Auslandsaufenthalt",
+        answersList: [
+            {answer:"ist nonverbale Kommunikation besonders wichtig.", isCorrect:false},
+            {answer:"verfügen Sie über umfangreiche Kenntnisse.", isCorrect:false},
+            {answer:"können sie Ihre Sprachkenntnisse vertiefen.", isCorrect:true},
+        ]
+    },{
+        question:"Umgangsprachliche Ausdrücke sind vor allem",
+        answersList: [
+            {answer:"bei Vorstellungsgesprächen zu vermeiden.", isCorrect:true},
+            {answer:"bei der Suche nach Informationen hilfreich.", isCorrect:false},
+            {answer:"bei Auslandsaufenthalten von geringer Bedeutung.", isCorrect:false},
+        ]
+    },{
+        question:"Digitale Seminarveranstaltungen",
+        answersList: [
+            {answer:"verbessern Ihre Kompetenzen beim Lernen.", isCorrect:false},
+            {answer:"erfreuen sich immer größerer Beliebtheit.", isCorrect:true},
+            {answer:"unterstützen die beliebten Dozenten.", isCorrect:false},
+        ]
+    },{
+        question:"Hätte ich mich besser auf den Test vorbereitet,",
+        answersList: [
+            {answer:"könnte ich jetzt besser laufen.", isCorrect:false},
+            {answer:"ist alles bei mir schief gelaufen.", isCorrect:false},
+            {answer:"wäre es nicht schief gelaufen.", isCorrect:true},
+        ]
+    },{
+        question:"Du hast nur noch wenig Zeit.",
+        answersList: [
+            {answer:"Du solltest endlich eine Entscheidung treffen.", isCorrect:true},
+            {answer:"Du konntest letztendlich nichts entscheiden.", isCorrect:false},
+            {answer:"Ich übernehme daher keine Verantwortung.", isCorrect:false},
+        ]
+    },{
+        question:"Wir wohnen noch nicht lange in der Stadt.",
+        answersList: [
+            {answer:"Folglich werden wir wenig Erfahrungen machen.", isCorrect:false},
+            {answer:"Daraus folgt, dass wir viele Restaurants gefunden haben.", isCorrect:false},
+            {answer:"Infolgedessen kennen wir kaum gute Restaurants.", isCorrect:true},
+        ]
+    },
+
+].sort(() => Math.random() - 0.5);
+
+export default QuestionsList;
